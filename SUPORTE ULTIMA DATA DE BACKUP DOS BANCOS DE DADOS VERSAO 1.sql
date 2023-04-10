3ee2eb3f2282ece2a9e7354b588a1715d5f66ecf
Cloud DB <44365807+CloudDBM@users.noreply.github.com>
2023-04-10 18:14:30 -0300

    Delete SUPORTE ULTIMA DATA DE BACKUP DOS BANCOS DE DADOS VERSAO 1.sql

--- SUPORTE ULTIMA DATA DE BACKUP DOS BANCOS DE DADOS VERSAO 1.sql
+++
@@ -1,5 +0,0 @@
-SELECT sdb.Name AS DatabaseName,
-COALESCE(CONVERT(VARCHAR(12), MAX(bus.backup_finish_date), 101),'-') AS LastBackUpTime
-FROM sys.sysdatabases sdb
-LEFT OUTER JOIN msdb.dbo.backupset bus ON bus.database_name = sdb.name
-GROUP BY sdb.Name
