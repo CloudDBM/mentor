b703b8100373f8a50dcff86df010986d87310f3c
Cloud DB <44365807+CloudDBM@users.noreply.github.com>
2023-04-10 18:28:03 -0300

    Delete SUPORTE ENVIA EMAIL SEM PROFILE VERSAO 1.sql

--- SUPORTE ENVIA EMAIL SEM PROFILE VERSAO 1.sql
+++
@@ -1,5 +0,0 @@
-EXEC msdb.dbo.sp_send_dbmail 
-@recipients='vpmaciel@gmail.com;monitoramento@clouddbm.com',  
-@subject = 'Enviando email sem profile',  
-@body = 'Enviando email sem profile',  
-@body_format = 'HTML' ;
