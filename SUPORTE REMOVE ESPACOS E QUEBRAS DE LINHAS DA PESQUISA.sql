f67da445d8e3a7b2273e7919310656a7ba8bb047
Cloud DB <44365807+CloudDBM@users.noreply.github.com>
2023-04-10 18:16:23 -0300

    Delete SUPORTE REMOVE ESPACOS E QUEBRAS DE LINHAS DA PESQUISA.sql

--- SUPORTE REMOVE ESPACOS E QUEBRAS DE LINHAS DA PESQUISA.sql
+++
@@ -1,4 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [NOME]
-      ,LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(CAST(NOME AS VARCHAR(MAX)), CHAR(9), ''), CHAR(10), ''), CHAR(13), ''))) as NOME
-  FROM [TESTE].[dbo].[ALUNO]
